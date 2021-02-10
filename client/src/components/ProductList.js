5da11fc719bd9996641beaccf2dea5ee7cb0b6ec
Daliabulatifa <74101819+Daliabulatifa@users.noreply.github.com>
2021-02-10 14:47:17 +0200

    product manager assignment

---
+++ client/src/components/ProductList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import axios from 'axios';
+export default props => {
+    return (
+        <div>
+            {props.product.map((product, idx)=>{
+                return <p key={idx}>{product.title}, {product.description}, {product.price}</p>
+            })}
+        </div>
+    )
+}
---
+++ server/server/routes/product.routes.js
@@ -0,0 +1,10 @@
+const ProductController = require('../controllers/product.controller');
+module.exports = function(app){
+    app.post('/api/product', ProductController.createProduct);
+    app.get('/api/product', ProductController.getAllProduct);
+    app.get('/api/people/:id', ProductController.getProduct);
+
+
+}
+
+
